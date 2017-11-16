ebb5c437efadd24f96e7d3c9756b4f1cbef1732d
Demis Bellot <demis.bellot@gmail.com>
2017-11-16 07:00:30 -0500

    remove unnecessary FallbackForClientRoutes

--- MyApp.ServiceInterface/MyServices.cs
+++ MyApp.ServiceInterface/MyServices.cs
@@ -8,19 +8,8 @@ using MyApp.ServiceModel;
 
 namespace MyApp.ServiceInterface
 {
-    [Exclude(Feature.Metadata)]
-    [FallbackRoute("/{PathInfo*}", Matches="AcceptsHtml")]
-    public class FallbackForClientRoutes
-    {
-        public string PathInfo { get; set; }
-    }
-
     public class MyServices : Service
     {
-        //Return index.html for unmatched requests so routing is handled on client
-        public object Any(FallbackForClientRoutes request) => 
-            new PageResult(Request.GetPage("/"));
-
         public object Any(Hello request)
         {
             return new HelloResponse { Result = $"Hello, {request.Name}!" };
